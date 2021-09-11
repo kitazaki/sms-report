39adfded174117a706dbaf26fd5221383c883d55
Ayachika Kitazaki <kitazaki@gmail.com>
2021-09-11 23:42:29 +0900

    Add files via upload

--- sms.sh
+++ sms.sh
@@ -6,11 +6,12 @@ do
 
   # clear input.csv file
   if [ -e input.csv ]; then
+	cat input.csv >> input.log
 	rm input.csv
   fi
 
   # check SMS message
-  python sms3_run.py > input.csv
+  python sms4_run.py > input.csv
 
   # update Google SpreadSheet
   if [ -s input.csv ]; then
